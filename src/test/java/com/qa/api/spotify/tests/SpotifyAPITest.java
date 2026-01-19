5f0355ed7488aeee6115f5114a64b2cdc458bf36
Nitya <nityapractice@gmail.com>
2026-01-19 23:05:53 +0530

    test: spotify test code is pushed

--- src/test/java/com/qa/api/spotify/tests/SpotifyAPITest.java
+++ src/test/java/com/qa/api/spotify/tests/SpotifyAPITest.java
@@ -1,5 +1,12 @@
 package com.qa.api.spotify.tests;
 
-public class SpotifyAPITest {
+import org.testng.annotations.BeforeTest;
 
+import com.qa.api.base.BaseTest;
+
+public class SpotifyAPITest extends BaseTest{
+	@BeforeTest
+	public void getAccessToken() {
+		restClient.post(BASE_URL_SPOTIFY, SPOTIFY_TOKEN_ENDPOINT, BASE_URL_SPOTIFY, BASE_URL_HEROKU_BASIC_AUTH, BASE_URL_GOREST, null);
+	}
 }
